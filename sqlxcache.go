f531a9ef96c0ddd2cfa72873326ccdde91433345
Jacob Lewallen <jlewallen@gmail.com>
2018-04-20 09:34:12 -0700

    Transaction related updates.

--- sqlxcache.go
+++ sqlxcache.go
@@ -39,13 +39,45 @@ func (db *DB) cacheStmt(query string) (*sqlx.Stmt, error) {
 	db.cache[query] = stmt
 	return stmt, nil
 }
+func (db *DB) cacheStmtContext(ctx context.Context, query string) (*sqlx.Stmt, error) {
+	tx := db.Transaction(ctx)
+	if tx != nil {
+		stmt, err := tx.Preparex(query)
+		if err != nil {
+			return nil, err
+		}
+		return stmt, nil
+	}
+	stmt, err := db.db.PreparexContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	return stmt, nil
+}
 
+func (db *DB) cacheNamedStmtContext(ctx context.Context, query string) (*sqlx.NamedStmt, error) {
+	tx := db.Transaction(ctx)
+	if tx != nil {
+		namedStmt, err := tx.PrepareNamed(query)
+		if err != nil {
+			return nil, err
+		}
+		return namedStmt, nil
+	}
+	namedStmt, err := db.db.PrepareNamedContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	return namedStmt, nil
+}
+
+/*
 func (db *DB) cacheStmtContext(ctx context.Context, query string) (*sqlx.Stmt, error) {
 	db.mu.Lock()
 	defer db.mu.Unlock()
 
 	if stmt, ok := db.cache[query]; ok {
-		return stmt, nil
+		return db.stmtWithTx(ctx, stmt), nil
 	}
 
 	stmt, err := db.db.PreparexContext(ctx, query)
@@ -54,24 +86,7 @@ func (db *DB) cacheStmtContext(ctx context.Context, query string) (*sqlx.Stmt, e
 	}
 
 	db.cache[query] = stmt
-	return stmt, nil
-}
-
-func (db *DB) cacheNamedStmt(query string) (*sqlx.NamedStmt, error) {
-	db.mu.Lock()
-	defer db.mu.Unlock()
-
-	if namedStmt, ok := db.cacheNamed[query]; ok {
-		return namedStmt, nil
-	}
-
-	namedStmt, err := db.db.PrepareNamed(query)
-	if err != nil {
-		return nil, err
-	}
-
-	db.cacheNamed[query] = namedStmt
-	return namedStmt, nil
+	return db.stmtWithTx(ctx, stmt), nil
 }
 
 func (db *DB) cacheNamedStmtContext(ctx context.Context, query string) (*sqlx.NamedStmt, error) {
@@ -79,7 +94,7 @@ func (db *DB) cacheNamedStmtContext(ctx context.Context, query string) (*sqlx.Na
 	defer db.mu.Unlock()
 
 	if namedStmt, ok := db.cacheNamed[query]; ok {
-		return namedStmt, nil
+		return db.namedStmtWithTx(ctx, namedStmt), nil
 	}
 
 	namedStmt, err := db.db.PrepareNamedContext(ctx, query)
@@ -88,7 +103,24 @@ func (db *DB) cacheNamedStmtContext(ctx context.Context, query string) (*sqlx.Na
 	}
 
 	db.cacheNamed[query] = namedStmt
-	return namedStmt, nil
+	return db.namedStmtWithTx(ctx, namedStmt), nil
+}
+*/
+
+func (db *DB) stmtWithTx(ctx context.Context, stmt *sqlx.Stmt) *sqlx.Stmt {
+	tx := db.Transaction(ctx)
+	if tx == nil {
+		return stmt
+	}
+	return tx.Stmtx(stmt)
+}
+
+func (db *DB) namedStmtWithTx(ctx context.Context, stmt *sqlx.NamedStmt) *sqlx.NamedStmt {
+	tx := db.Transaction(ctx)
+	if tx == nil {
+		return stmt
+	}
+	return tx.NamedStmt(stmt)
 }
 
 func Connect(driverName, dataSourceName string) (*DB, error) {
@@ -121,9 +153,6 @@ func Open(driverName, dataSourceName string) (*DB, error) {
 	return newDB(db), nil
 }
 
-// func (db *DB) Beginx() (*sqlx.Tx, error)
-// func (db *DB) BindNamed(query string, arg interface{}) (string, []interface{}, error)
-
 func (db *DB) DriverName() string {
 	return db.db.DriverName()
 }
@@ -135,6 +164,15 @@ func (db *DB) Get(dest interface{}, query string, args ...interface{}) error {
 	}
 
 	return stmt.Get(dest, args...)
+}
+
+func (db *DB) Select(dest interface{}, query string, args ...interface{}) error {
+	stmt, err := db.cacheStmt(query)
+	if err != nil {
+		return err
+	}
+
+	return stmt.Select(dest, args...)
 }
 
 func (db *DB) GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
@@ -155,30 +193,6 @@ func (db *DB) ExecContext(ctx context.Context, query string, args ...interface{}
 	return stmt.ExecContext(ctx, args...)
 }
 
-// func (db *DB) MapperFunc(mf func(string) string)
-func (db *DB) MustBegin() *sqlx.Tx {
-	return db.db.MustBegin()
-}
-
-// func (db *DB) MustExec(query string, args ...interface{}) sql.Result {
-// 	stmt, err := db.cacheStmt(query)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return stmt.MustExec(args...)
-// }
-
-// not sqlx
-func (db *DB) NamedGet(dest interface{}, query string, arg interface{}) error {
-	namedStmt, err := db.cacheNamedStmt(query)
-	if err != nil {
-		return err
-	}
-
-	return namedStmt.Get(dest, arg)
-}
-
 // not sqlx
 func (db *DB) NamedGetContext(ctx context.Context, dest interface{}, query string, arg interface{}) error {
 	namedStmt, err := db.cacheNamedStmtContext(ctx, query)
@@ -187,15 +201,6 @@ func (db *DB) NamedGetContext(ctx context.Context, dest interface{}, query strin
 	}
 
 	return namedStmt.GetContext(ctx, dest, arg)
-}
-
-func (db *DB) NamedExec(query string, arg interface{}) (sql.Result, error) {
-	namedStmt, err := db.cacheNamedStmt(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return namedStmt.Exec(arg)
 }
 
 func (db *DB) NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error) {
@@ -207,15 +212,6 @@ func (db *DB) NamedExecContext(ctx context.Context, query string, arg interface{
 	return namedStmt.ExecContext(ctx, arg)
 }
 
-func (db *DB) NamedQuery(query string, arg interface{}) (*sqlx.Rows, error) {
-	namedStmt, err := db.cacheNamedStmt(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return namedStmt.Queryx(arg)
-}
-
 func (db *DB) NamedQueryContext(ctx context.Context, query string, arg interface{}) (*sqlx.Rows, error) {
 	namedStmt, err := db.cacheNamedStmtContext(ctx, query)
 	if err != nil {
@@ -225,17 +221,6 @@ func (db *DB) NamedQueryContext(ctx context.Context, query string, arg interface
 	return namedStmt.QueryxContext(ctx, arg)
 }
 
-// func (db *DB) QueryRowx(query string, args ...interface{}) *sqlx.Row
-
-func (db *DB) Queryx(query string, args ...interface{}) (*sqlx.Rows, error) {
-	stmt, err := db.cacheStmt(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return stmt.Queryx(args...)
-}
-
 func (db *DB) QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error) {
 	stmt, err := db.cacheStmtContext(ctx, query)
 	if err != nil {
@@ -243,19 +228,6 @@ func (db *DB) QueryxContext(ctx context.Context, query string, args ...interface
 	}
 
 	return stmt.QueryxContext(ctx, args...)
-}
-
-func (db *DB) Rebind(query string) string {
-	return db.db.Rebind(query)
-}
-
-func (db *DB) Select(dest interface{}, query string, args ...interface{}) error {
-	stmt, err := db.cacheStmt(query)
-	if err != nil {
-		return err
-	}
-
-	return stmt.Select(dest, args...)
 }
 
 func (db *DB) SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
@@ -269,4 +241,33 @@ func (db *DB) SelectContext(ctx context.Context, dest interface{}, query string,
 
 func (db *DB) Unsafe() *DB {
 	return newDB(db.db.Unsafe())
+}
+
+type transactionContextKey string
+
+var TxContextKey = transactionContextKey("sqlx.tx")
+
+func (db *DB) WithNewTransaction(ctx context.Context, fn func(context.Context) error) error {
+	tx, err := db.db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	txCtx := context.WithValue(ctx, TxContextKey, tx)
+	err = fn(txCtx)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+
+	return err
+}
+
+func (db *DB) Transaction(ctx context.Context) (tx *sqlx.Tx) {
+	if v := ctx.Value(TxContextKey); v != nil {
+		return v.(*sqlx.Tx)
+	}
+	return nil
 }
